a2cf23592891ed71c3fa8e4e86dc7546906d5dff
MICHAEL GURLEY <michaeljamesgurley@gmail.com>
2018-01-22 14:22:35 -0500

    Debugging mobile iphone.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,10 @@
 //= require materialize
 //= require_tree .
 
-
 $(document).ready(function(){
+
+});
+
+$(document).on('turbolinks:load', function() {
   $('.carousel.carousel-slider').carousel();
 });
